46d0f147fd81472013e4ade403a61039a61b23c2
jmcanog03 <jmcanog03@educarex.es>
2023-01-16 19:37:37 +0100

    v1.12

---
+++ hotel.sql
@@ -0,0 +1,30 @@
+CREATE TABLE hotel(
+    Nombre VARCHAR(12),
+    Direccion VARCHAR(15),
+   	Teléfono VARCHAR(9),
+    WEB VARCHAR (20),
+    categoria VARCHAR(10),
+    nºdehabitaciones INT,
+    ID_Hotel INT,
+    PRIMARY KEY (ID_Hotel)
+    
+);
+
+CREATE TABLE Habitación(
+    ID_Habitación INT,
+    Precio INT,
+    nºdehabitaciones ENUM(1-510),
+    Capacidad INT,º
+    Extras VARCHAR(10)
+   INSERT INTO Habitación (  ) VALUES(
+        your_values
+   );
+);
+-- hola 
+CREATE TABLE Reserva(
+    fecha de la Reserva DATE,
+    nombre del hotel  VARCHAR(15)
+    fecha de entrada DATE
+    numero de coches INT
+);
+
